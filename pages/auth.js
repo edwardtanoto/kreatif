62b8e11614e0d314fb5856f17d87eb8fc120a80e
edwardtanoto <edwardt.dev@gmail.com>
2022-02-28 02:44:49 +0700

    gege

---
+++ pages/auth.js
@@ -0,0 +1,145 @@
+import { auth,firestore, googleAuthProvider } from '../lib/firebase';
+import { UserContext } from '../lib/context';
+
+import { useEffect, useState, useCallback, useContext } from 'react';
+import debounce from 'lodash.debounce';
+
+const AuthPage = () => {
+  const { user, username } = useContext(UserContext)
+
+  return (
+    <main>
+      {user ? !username ? <>
+      <Test />
+      <UsernameForm />
+      </> : <SignOutButton /> : <SignInButton />}
+  </main>
+  )
+}
+
+// Sign in with Google button
+function SignInButton() {
+    const signInWithGoogle = async () => {
+      await auth.signInWithPopup(googleAuthProvider);
+    };
+  
+    return (
+      <button className="btn-google" onClick={signInWithGoogle}>
+        <img src={'/google.png'} /> Sign in with Google
+      </button>
+    );
+  }
+  
+  // Sign out button
+  function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>;
+  }
+
+function Test(){
+    return <p>Halo tewz</p>
+}
+  
+  
+// Username form
+function UsernameForm() {
+    const [userName, setUserName] = useState('');
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
+  
+    const { user, username } = useContext(UserContext);
+  
+    const onSubmit = async (e) => {
+      e.preventDefault();
+  
+      // Create refs for both documents
+      const userDoc = firestore.doc(`users/${user.uid}`);
+      const usernameDoc = firestore.doc(`usernames/${userName}`);
+  
+      // Commit both docs together as a batch write.
+      const batch = firestore.batch();
+      batch.set(userDoc, { username: userName, photoURL: user.photoURL, displayName: user.displayName });
+      batch.set(usernameDoc, { uid: user.uid });
+  
+      await batch.commit();
+    };
+  
+    const onChange = (e) => {
+      // Force form value typed in form to match correct format
+      const val = e.target.value.toLowerCase();
+      const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  
+      // Only set form value if length is < 3 OR it passes regex
+      if (val.length < 3) {
+        setUserName(val);
+        setLoading(false);
+        setIsValid(false);
+      }
+  
+      if (re.test(val)) {
+        setUserName(val);
+        setLoading(true);
+        setIsValid(false);
+      }
+    };
+  
+    //
+  
+    useEffect(() => {
+      checkUsername(userName);
+    }, [userName]);
+  
+    // Hit the database for username match after each debounced change
+    // useCallback is required for debounce to work
+    const checkUsername = useCallback(
+      debounce(async (username) => {
+        if (username.length >= 3) {
+          const ref = firestore.doc(`usernames/${username}`);
+          const { exists } = await ref.get();
+          console.log('Firestore read executed!');
+          setIsValid(!exists);
+          setLoading(false);
+        }
+      }, 500),
+      []
+    );
+  
+    return (
+      !username && (
+        <section>
+          <h3>Choose Username</h3>
+          <form onSubmit={onSubmit}>
+            <input name="username" placeholder="myname" value={userName} onChange={onChange} />
+            <UsernameMessage username={userName} isValid={isValid} loading={loading} />
+            <button type="submit" className="btn-green" disabled={!isValid}>
+              Choose
+            </button>
+  
+            <h3>Debug State</h3>
+            <div>
+              Username: {userName}
+              <br />
+              Loading: {loading.toString()}
+              <br />
+              Username Valid: {isValid.toString()}
+            </div>
+          </form>
+        </section>
+      )
+    );
+  }
+  
+  function UsernameMessage({ username, isValid, loading }) {
+    if (loading) {
+      return <p>Checking...</p>;
+    } else if (isValid) {
+      return <p className="text-success">{username} is available!</p>;
+    } else if (username.length < 3 && !isValid) {
+        return <p className="text-danger">Username must have more than 3 length</p>;
+    } else if (username && !isValid) {
+      return <p className="text-danger">That username is taken!</p>;
+    } else {
+      return <p></p>;
+    }
+  }
+
+export default AuthPage
