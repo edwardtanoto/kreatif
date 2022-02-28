d4c510c7e34997cce529fae77e901cc101c0d202
edwardtanoto <edwardt.dev@gmail.com>
2022-02-28 22:44:05 +0700

    add thumbnail

--- components/UserProfileDetail.js
+++ components/UserProfileDetail.js
@@ -5,8 +5,9 @@ const UserProfileDetail = ({user}) => {
   return (
     <div className='user-profile-detail'>
         <img src={user.photoURL} className="card-img-center" />
-        <h1> {user.bio}</h1>
-        <p>{user.jobTitle} based in {user.country}</p>
+        <h1> {user.displayName}</h1>
+        <h3>{user.jobTitle} based in {user.country}</h3>
+        <p>{user.bio}</p>
         <AuthCheck
           fallback={
             <Link href="/auth" className='middle'>
