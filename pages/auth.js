179d3125fd03c4bd0a7c2a2c50c8b6ffd6cc5568
edwardtanoto <edwardt.dev@gmail.com>
2022-02-28 11:48:16 +0700

    form validation onboarding complete

--- pages/auth.js
+++ pages/auth.js
@@ -1,5 +1,6 @@
 import { auth,firestore, googleAuthProvider } from '../lib/firebase';
 import { UserContext, UserFormContext } from '../lib/context';
+import { useForm } from 'react-hook-form';
 
 import { useEffect, useState, useCallback, useContext } from 'react';
 import debounce from 'lodash.debounce';
@@ -7,11 +8,10 @@ import Title from '../components/Title';
 
 const AuthPage = () => {
   const { user, username } = useContext(UserContext)
-  const {userform} = useContext(UserFormContext);
   return (
     <main>
       {user ? !username ? <>
-      <Title title={'Onboarding'}/>
+      <Title title={`Hello`}/>
       <UsernameForm />
       </> : <SignOutButton /> :  <SignInButton />}
   </main>
@@ -45,7 +45,11 @@ function SignInButton() {
   
 // Username form
 function UsernameForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    console.log(errors);
+
     const [userName, setUserName] = useState('');
+
     const [isValid, setIsValid] = useState(false);
     const [loading, setLoading] = useState(false);
   
@@ -56,6 +60,7 @@ function UsernameForm() {
         if(userform) setUserName(userform);
     }, [userform])
     
+    const onSubmitOnboarding = data => console.log(data);
   
     const onSubmit = async (e) => {
       e.preventDefault();
@@ -116,39 +121,78 @@ function UsernameForm() {
     return (
       !username && (
         <section>
-        
-        <label>Choose Username</label>
-        <br/>
-        <br/>
           <div>
-          <form onSubmit={onSubmit} className='username-form'>
-              <label className='middle'>kreatif.app/</label>
-              
-            <input className='middle' name="username" placeholder={!userform ? 'myname' : `${userform}`} value={userName} onChange={onChange} />
-            
-           
-            {/* <h3>Debug State</h3>
-            <div>
-              Username: {userName}
-              <br />
-              Loading: {loading.toString()}
-              <br />
-              Username Valid: {isValid.toString()}
-            </div> */}
-            <button type='submit' className='btn-green'>Confirm</button>
-          </form>
+          <form onSubmit={handleSubmit(onSubmitOnboarding)}>
+            <div className='onboarding-form'>
+              <label>Username</label>
+            <input name="username" placeholder={!userform ? 'myname' : `${userform}`} value={userName} onChange={onChange} />
+            </div>
+            <UsernameMessage username={userName} isValid={isValid} loading={loading} />
           
-         <br/>
-          <input className='middle' name="firstname" value={userName} onChange={onChange} />
-          <input className='middle' name="second name" value={userName} onChange={onChange} />
-          <input className='middle' name="firstname" value={userName} onChange={onChange} />
-          <input className='middle' name="work" value={userName} onChange={onChange} />
-          <input className='middle' name="lokasi" value={userName} onChange={onChange} />
-          <input className='middle' name="budget" value={userName} onChange={onChange} />
-              
+            {/* <button type='submit' className='btn-green'>Confirm</button> */}
+ 
+          <div className='onboarding-form'>
+          <label>Nama Depan</label>
+            <input type="text" placeholder="First name" {...register('firstName', { minLength: { value: 2, message: "First Name is too short" }, required: {value:true, message:"Nama Depan kosong, mohon diisi."} })} />
+            {errors.firstName && <p className="text-danger">{errors.firstName.message}</p>}
+      </div>
+      <div className='onboarding-form'>
+          <label>Nama Belakang</label>
+          <input type="text" placeholder="Last name" {...register('secondName', { minLength: { value: 2, message: "Second Name is too short" } , required: {value:true, message:"Nama Belakang kosong, mohon diisi." }})} />
+          {errors.secondName && <p className="text-danger">{errors.secondName.message}</p>}
+      </div>
+      <div className='onboarding-form'>
+          <label>Employment Type</label>
+        <select {...register('employmentType', { minLength: { value: 2, message: "Employment Type is too short" } })}>
+        <option value="Full-Time">Full-Time</option>
+        <option value="Part-Time">Part-Time</option>
+        <option value="Self-Employed">Self-Employed</option>
+        <option value="Freelance">Freelance</option>
+        <option value="Contract">Contract</option>
+        <option value="Internship">Internship</option>
+        <option value="Apprenticeship">Apprenticeship</option>
+      </select>
+      {errors.employmentType && <p className="text-danger">{errors.employmentType.message}</p>}
+      </div>
+      <div className='onboarding-form'>
+          <label>Job Title</label>
+      <select {...register("Job Title", { required: true })}>
+        <option value="UI/UX Design">UI/UX Design</option>
+        <option value="Graphic Design">Graphic Design</option>
+        <option value="Software Developer">Software Developer</option>
+        <option value="Web Developer">Web Developer</option>
+        <option value="App Developer">App Developer</option>
+        <option value="Digital Marketing">Digital Marketing</option>
+        <option value="Others">Others</option>
+      </select>
+      </div>
+      <div className='onboarding-form'>
+          <label>Perusahaan</label>
+          <input type="text" placeholder="Google" {...register("company",  { minLength: { value: 2, message: "Masukkan Perusahaan yang tepat." } , required: {value:true, message:"Perusahaan anda kosong, mohon diisi." }})} />
+          {errors.company && <p className="text-danger">{errors.company.message}</p>}
+      </div>
+      <div className='onboarding-form'>
+          <label>Kota</label>
+          <input type="text" placeholder="Jakarta" {...register("city", { minLength: { value: 2, message: "Masukkan Kota yang tepat." } , required: {value:true, message:"Mohon diisi Kota asal." }})} />
+          {errors.city && <p className="text-danger">{errors.city.message}</p>}
+      </div>
+      <div className='onboarding-form'>
+          <label>Negara</label>
+          <input type="text" placeholder="Indonesia" {...register("country", { minLength: { value: 2, message: "Masukkan Negara yang tepat." } , required: {value:true, message:"Mohon diisi Negara asal." }})} />
+          {errors.country && <p className="text-danger">{errors.country.message}</p>}
+      </div>
+     
+     
+     
+      
+     
+     
+
+      <input type="submit" />
+    </form>
   
            
-          </div> <UsernameMessage username={userName} isValid={isValid} loading={loading} />
+          </div> 
          
         </section>
       )
@@ -161,9 +205,9 @@ function UsernameForm() {
     } else if (isValid) {
       return <p className="text-success">{username} is available!</p>;
     } else if (username.length < 3 && !isValid) {
-        return <p className="text-danger">Username must have more than 3 length</p>;
+        return <p className="text-danger">Jumlah username harus diatas angka tiga.</p>;
     } else if (username && !isValid) {
-      return <p className="text-danger">That username is taken!</p>;
+      return <p className="text-danger">Username tersebut telah diambil.</p>;
     } else {
       return <p></p>;
     }
