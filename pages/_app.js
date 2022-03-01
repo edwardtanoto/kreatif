60464df5137fea04c1bf6141125103cbe273622d
edwardtanoto <edwardt.dev@gmail.com>
2022-03-01 20:42:36 +0700

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,20 @@ import '../styles/globals.css'
 
 import { UserContext, UserFormProvider } from '../lib/context';
 import { useUserData } from '../lib/hooks';
+import NextNProgress from 'nextjs-progressbar';
+import { Progress } from '../components/progress';
 
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
+
   return <>
      <UserContext.Provider value={userData}>
        <UserFormProvider>
       <Navbar />
+      <NextNProgress/>
+
       <Component {...pageProps} />
       </UserFormProvider>
     </UserContext.Provider>
